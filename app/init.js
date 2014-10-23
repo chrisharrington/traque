6801e313568ce361a2f3535cb13ed3cc48f05da9
Chris Harrington <chrisharrington99@gmail.com>
2014-10-22 22:22:46 -0600

    Removed base tag.

--- app/init.js
+++ app/init.js
@@ -5,6 +5,4 @@ app.config(function($routeProvider, $locationProvider) {
 	$routeProvider
 		.when("/test", { templateUrl: "pages/test.html", controller: "test" })
 		.otherwise({ redirectTo: "/test" });
-	
-	$locationProvider.html5Mode(true);
 });
--- public/js/app.js
+++ public/js/app.js
@@ -134,8 +134,6 @@ app.config(function($routeProvider, $locationProvider) {
 	$routeProvider
 		.when("/test", { templateUrl: "pages/test.html", controller: "test" })
 		.otherwise({ redirectTo: "/test" });
-	
-	$locationProvider.html5Mode(true);
 });
 });
 
